9c7b504422d1dd8bb61715ba809b1f6cc12899e8
Jaime Sarrion <jaime.sarrion@gmail.com>
2023-11-07 17:46:40 +0100

    add header component

--- monorepo/packages/customer-app/src/App.tsx
+++ monorepo/packages/customer-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { getAllProducts } from "@finand-edi/shared-logic";
 import ProductLists from "./components/product-list/product-list";
+import Header from "./components/header/header";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -18,12 +19,8 @@ function App() {
   }
   return (
     <div className="App">
-      <header>
-        <h1>Nombre de la empresa</h1>
-      </header>
-      <div>
-        <ProductLists items={items} />
-      </div>
+      <Header />
+      <ProductLists items={items} />
     </div>
   );
 }
---
+++ monorepo/packages/customer-app/src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as Styled from "./header.styled";
+
+const Header: React.FC = () => {
+  return (
+    <Styled.HContainer>
+      <h1>Gedesco</h1>
+    </Styled.HContainer>
+  );
+};
+
+export default Header;
