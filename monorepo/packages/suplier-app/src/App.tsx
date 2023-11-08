72cdb4111ab7c389ae53a30f81de4f702fe3e889
Jaime Sarrion <jaime.sarrion@gmail.com>
2023-11-08 17:53:24 +0100

    add products table on the app component

--- monorepo/packages/suplier-app/src/App.tsx
+++ monorepo/packages/suplier-app/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getAllProducts } from "@finand-edi/shared-logic";
+import "./App.css";
+import ProductTable from "./components/products-table/products-table";
+import { Item } from "./types/Items";
+
 
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
+  async function getAllItems() {
+    const products = await getAllProducts();
+    if (products) {
+      setItems(products);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductTable 
+        items={items}
+        itemsPerPage={10}
+      />
     </div>
   );
 }
