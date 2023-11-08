09b6b819e68beabef3cb2c33114aa1bf176d7d96
Jaime Sarrion <jaime.sarrion@gmail.com>
2023-11-08 18:13:09 +0100

    add typography to the title

--- monorepo/packages/customer-app/src/components/header/header.styled.ts
+++ monorepo/packages/customer-app/src/components/header/header.styled.ts
@@ -4,4 +4,7 @@ import styled from 'styled-components'
 export const HContainer = styled.header`
     display: flex;
     justify-content: center;
+    font-size: 24px;
+    font-family: Avenir-Heavy;
+    font-weight: 800;
 `;
