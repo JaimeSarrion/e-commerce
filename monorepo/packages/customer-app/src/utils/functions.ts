14f85b0074ad8ca1df9c5d795bcd4e3cbe61a905
Jaime Sarrion <jaime.sarrion@gmail.com>
2023-11-08 12:28:50 +0100

    function to sort by price

--- monorepo/packages/customer-app/src/utils/functions.ts
+++ monorepo/packages/customer-app/src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { Item } from "../types/Items";
+
 export function capitalizeFirstLetterInArray(inputArray: string[]): { label: string; id: string }[] {
     const resultArray = inputArray.map((item) => ({
       label: item.charAt(0).toUpperCase() + item.slice(1),
@@ -5,4 +7,14 @@ export function capitalizeFirstLetterInArray(inputArray: string[]): { label: str
     }));
   
     return resultArray;
+  }
+
+  export function sortItemsByPrice(items: Item[], isAscending: boolean): Item[] {
+    return items.slice().sort((a, b) => {
+      if (isAscending) {
+        return a.price - b.price;
+      } else {
+        return b.price - a.price;
+      }
+    });
   }
